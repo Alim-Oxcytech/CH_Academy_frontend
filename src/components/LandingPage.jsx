f1b98e2a5451493dc91cf33e6a9d124ddb5a6910
Alim-Oxcytech <alim.mohd@oxcytech.com>
2024-08-24 16:10:35 +0530

    first commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import AppAppBar from "./AppAppBar";
+import Hero from "./Hero";
+import LogoCollection from "./LogoCollection";
+import Highlights from "./Highlights";
+import Pricing from "./Pricing";
+import Features from "./Features";
+import Testimonials from "./Testimonials";
+import FAQ from "./FAQ";
+import Footer from "./Footer";
+import getLPTheme from "./getLPTheme";
+
+export default function LandingPage() {
+  const [mode, setMode] = React.useState("light");
+  const LPtheme = createTheme(getLPTheme(mode));
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  return (
+    <ThemeProvider theme={LPtheme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Hero />
+      <Box sx={{ bgcolor: "background.default" }}>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box>
+    </ThemeProvider>
+  );
+}
